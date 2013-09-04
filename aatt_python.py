fb73a5041182120bb7fb17d1a490d6fb24473bb8
DoriftoShoes <doriftoshoes@marshmallowkittens.org>
2013-09-04 14:14:29 -0700

    Added documentation

--- aatt_python.py
+++ aatt_python.py
@@ -4,7 +4,9 @@ import json
 import requests
 
 class Aatt:
-
+	"""
+	Automate All The Things Python Module
+	"""
 	def __init__(self):
 		self.aattUrl = 'https://jarvis.marshmallowkittens.org/sync'
 		self.post = {}
@@ -13,24 +15,45 @@ class Aatt:
 		self.checks = {}
 		
 	def setSyncUrl(self,url):
+		"""
+		This is used to change the sync url.  It can also be set by hand in the aatt_python.py file.
+		"""
 		self.aattUrl = url
 
 	def setAccount(self,acctId,acctKey):
+		"""
+		All requests to the aatt server must include your account id and your account key.
+		"""
 		self.post.update({'APP':'aatt_python','ACCOUNT':acctId,'KEY':acctKey})
 
 	def setDevice(self,deviceId):
+		"""
+		Device ID must be specified for all requests, whether they are records or checks.
+		"""
 		self.post.update({'DEVICE':deviceId})
 
 	def setAction(self,action):
+		"""
+		Valid options for this method are RECORD or CHECK
+		"""
 		self.post.update({'ACT':action})
 
 	def record(self,item,value):
+		"""
+		If RECORD is set as the action you can add all the items you need to record with this method.  As many records can be included as needed and sent in a batch.  If you choose CHECK as your action, any records added will be ignored.
+		"""
 		self.records.update({'ITEM':item,'VALUE':value})
 
 	def check(self,item):
+		"""
+		If CHECK is set as teh action you can add all items to check with this method.  Checks can be batched by calling this as many times as necessary before calling send().  If RECORD is chosen as the action, these are ignored.
+		"""
 		self.checks.update({'ITEM':item})
 
 	def compile(self):
+		"""
+		This compiles all the various pieces into a properly formatted dict.  This does not need to be called separately from send.
+		"""
 		if self.action is 'RECORD':
 			self.post.update({'RECORDS':records})
 		elif self.action is 'CHECK':
@@ -38,6 +61,9 @@ class Aatt:
 		return self.post
 
 	def send(self):
+		"""
+		This compiles all the dicts into the proper format, converts them to json, then posts them to the aartUrl.
+		"""
 		self.compile()
 		self.payload = json.dumps(self.post)
 		r = requests.post(self.aattUrl,data=self.post)
