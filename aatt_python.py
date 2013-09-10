27b0feb564dcceb00df8e6e08df65fd6a4dfc40b
root <root@jarvis.marshmallowkittens.org>
2013-09-10 15:51:29 -0700

    Fixed table names for new schema.  Fixed check method for device>endpoint>attribute.

--- aatt_python.py
+++ aatt_python.py
@@ -2,7 +2,6 @@
 
 import json
 import requests
-import urllib2
 
 class Aatt:
 	"""
@@ -13,9 +12,11 @@ class Aatt:
 		self.auth = {}
 		self.data = {}
 		self.post = {}
-		self.action = {}
+		self.act = {}
 		self.records = {}
 		self.checks = {}
+		self.actions = {}
+		self.status = ''
 		
 	def setSyncUrl(self,url):
 		"""
@@ -27,7 +28,7 @@ class Aatt:
 		"""
 		All requests to the aatt server must include your account id and your account key.
 		"""
-		self.auth.update({'APP':'aatt','ACCOUNT':acctId,'KEY':acctKey})
+		self.auth.update({'APP':'aatt_python','ACCOUNT':acctId,'KEY':acctKey})
 
 	def setDevice(self,deviceId):
 		"""
@@ -35,34 +36,50 @@ class Aatt:
 		"""
 		self.data['DEVICE'] = deviceId
 
-	def setAction(self,action):
+	def setAct(self,act):
 		"""
-		Valid options for this method are RECORD or CHECK
+		Valid options for this method are RECORD, CHECK, ACTION, or REGISTER
 		"""
-		self.action = action
-		self.post['ACT'] = action
+		if act == 'RECORD' or act == 'CHECK' or act == 'ACTION' or act == 'REGISTER':
+			self.act = act
+			self.post['ACT'] = act
+		else:
+			self.status = 'BADACT'
 
-	def record(self,item,value):
+	def record(self,endpoint,value):
 		"""
 		If RECORD is set as the action you can add all the items you need to record with this method.  As many records can be included as needed and sent in a batch.  If you choose CHECK as your action, any records added will be ignored.
 		"""
-		self.records.update({item:value})
+		self.records.update({endpoint:value})
 
-	def check(self,item):
+	def check(self,endpoint,attribute):
 		"""
 		If CHECK is set as teh action you can add all items to check with this method.  Checks can be batched by calling this as many times as necessary before calling send().  If RECORD is chosen as the action, these are ignored.
 		"""
-		self.checks.update({item:item})
+		if endpoint in self.checks:
+			self.checks[endpoint].update({attribute:attribute})
+		else:
+			self.checks.update({endpoint:{attribute:attribute}})
+
+	def addAction(self,item,action):
+		"""
+		This method allows a device to set an action for another device.
+		"""
+		self.actions.update({item:action})
 
 	def compile(self):
 		"""
 		This compiles all the various pieces into a properly formatted dict.  This does not need to be called separately from send.
 		"""
 		self.post.update({'AUTH':self.auth})
-		if self.action is 'RECORD':
+		if self.act is 'RECORD':
 			self.data.update({'RECORDS':self.records})
-		elif self.action is 'CHECK':
+		elif self.act is 'CHECK':
 			self.data.update({'CHECKS':self.checks})
+		elif self.act is 'ACTION':
+			self.data.update({'ACTION':self.actions})
+		elif self.act is 'REGISTER':
+			self.data.update({'REGISTER':self.register})
 		self.post.update({'DATA':self.data})
 		return self.post
 
@@ -70,9 +87,12 @@ class Aatt:
 		"""
 		This compiles all the dicts into the proper format, converts them to json, then posts them to the aartUrl.
 		"""
-		self.compile()
-		payload = json.dumps(self.post)
-		header = {'content-type': 'application/json','User-Agent':'aatt Python'}
-		r = requests.post(self.aattUrl,data=payload,headers=header,verify=True)
-		return r.content
+		if self.status != 'BADACT':
+			self.compile()
+			payload = json.dumps(self.post)
+			header = {'content-type': 'application/json','User-Agent':'aatt Python'}
+			r = requests.post(self.aattUrl,data=payload,headers=header,verify=True)
+			return r.content
+		else:
+			return self.status
 		
